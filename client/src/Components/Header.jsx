c9c64d24e522802878afa36b9b85528549f10467
Arpit Surana <suranaarpit34@gmail.com>
2022-09-19 13:46:23 +0530

    init

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div className="headerbox">
+        <div className='headermain'>
+        <input type="text" />
+        <button>Search</button>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Header
